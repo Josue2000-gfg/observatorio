8db0242e41475fe710b3c5e19663adeef1c164d2
Josué Giménez <josuegimenez00@gmail.com>
2023-07-31 17:25:35 -0300

    Mejoras

--- script.js
+++ script.js
@@ -46,22 +46,34 @@ var myChart = new Chart(ctx, {
 });
 
 // Obtener elementos del DOM
-var infoDiv = document.querySelector('.home');
+var homeDiv = document.querySelector('.home');
 var cardDiv = document.querySelector('.card');
+var infoDiv = document.querySelector('.info');
 var inicioBtn = document.querySelector('.header .btn:nth-child(1)');
 var estadisticasBtn = document.querySelector('.header .btn:nth-child(2)');
+var nosotrosBtn = document.querySelector('.header .btn:nth-child(3)');
 
-// Ocultar el div de gráfico al cargar la página
+// Ocultar los divs de gráfico e información al cargar la página
 cardDiv.style.display = 'none';
+infoDiv.style.display = 'none';
 
-// Mostrar el div de información al presionar el botón de inicio
+// Mostrar el div de inicio al presionar el botón de inicio
 inicioBtn.addEventListener('click', function() {
-    infoDiv.style.display = 'block';
+    homeDiv.style.display = 'block';
     cardDiv.style.display = 'none';
+    infoDiv.style.display = 'none';
 });
 
 // Mostrar el div de gráfico al presionar el botón de estadísticas
 estadisticasBtn.addEventListener('click', function() {
-    infoDiv.style.display = 'none';
+    homeDiv.style.display = 'none';
     cardDiv.style.display = 'block';
+    infoDiv.style.display = 'none';
+});
+
+// Mostrar el div de información al presionar el botón de nosotros
+nosotrosBtn.addEventListener('click', function() {
+    homeDiv.style.display = 'none';
+    cardDiv.style.display = 'none';
+    infoDiv.style.display = 'block';
 });
